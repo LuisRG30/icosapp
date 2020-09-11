966437cd96ee940235db7c3ad7f22109c60244cc
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-09-10 23:13:15 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Icosapp.wsgi --log-file -
---
+++ landing/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+app_name = "landing"
+
+from . import views
+urlpatterns = [
+    path("", views.index, name="index")
+]
