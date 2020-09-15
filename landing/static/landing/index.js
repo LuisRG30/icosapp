224b239df7c9e89991c5581a788c8f45c320cae9
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-09-15 14:01:49 -0500

    finished animating header

--- landing/static/landing/index.js
+++ landing/static/landing/index.js
@@ -5,13 +5,23 @@ window.onscroll = function() {myFunction()};
 var header = document.getElementById("header");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = document.getElementById("three").offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.style.position = "fixed";
+  if (window.pageYOffset >= sticky) {
+    header.style.position = 'fixed';
+    header.classList.add("appear");
+    header.classList.remove("disappear");
   } else {
-    header.style.position = "absolute";
+    header.classList.remove("appear");
+    header.classList.add("disappear");
+    header.addEventListener("animationend", () => {
+        if (header.style.position == "fixed"){
+            header.style.position = "absolute";
+        } else{
+            header.style.position = "fixed";
+        }
+    });
   }
 }
