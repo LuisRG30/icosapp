ad14977c5c49cf83c5ccfe017715c8bd41df6ec0
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-09-15 11:21:00 -0500

    attempting sticky header

---
+++ landing/static/landing/index.js
@@ -0,0 +1,17 @@
+//Enable Header Scroll
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.style.position = "fixed";
+  } else {
+    header.style.position = "absolute";
+  }
+}
