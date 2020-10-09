6e38201c6ee5d82dda6b0fcf5be80cd67567635c
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-10-09 09:03:04 -0500

    Corrected grammar and added paths to urls.py

--- landing/urls.py
+++ landing/urls.py
@@ -5,5 +5,8 @@ app_name = "landing"
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("pricing/", views.pricing, name="pricing"),
+    path("select", views.select, name="select"),
+    path("contacto", views.contacto, name="contacto"),
+    path("desarrollo", views.desarrollo, name="desarrollo"),
+    path("pricing", views.pricing, name="pricing"),
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,5 +5,14 @@ from django.http import HttpResponse, JsonResponse
 def index(request):
     return render(request, "landing/index.html")
 
+def select(request):
+    return render(request, "landing/seleccion.html")
+
+def contacto(request):
+    return render(request, "landing/contacto.html")
+
+def desarrollo(request):
+    return render(request, "landing/desarrollo.html")
+
 def pricing(request):
     return render(request, "landing/pricing.html")
