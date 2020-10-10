2d88a0d512b7b70dc36eceb5564a5534415de13e
PabloOchoa00 <a01637785@itesm.mx>
2020-10-10 18:25:57 -0500

    cambios de developerluis

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+    message = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.name}, {self.email}"
--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, Http404, HttpResponseBadRequest
+
+from .models import Message
+from .forms import MessageForm
 
 # Create your views here.
+messageform = MessageForm()
+
 def index(request):
-    return render(request, "landing/index.html")
+    return render(request, "landing/index.html", {"messageform": messageform})
+
+def select(request):
+    return render(request, "landing/seleccion.html", {"messageform": messageform})
+
+def contacto(request):
+    return render(request, "landing/contacto.html", {"messageform": messageform})
+
+def desarrollo(request):
+    return render(request, "landing/desarrollo.html", {"messageform": messageform})
 
 def pricing(request):
-    return render(request, "landing/pricing.html")
+    return render(request, "landing/pricing.html", {"messageform": messageform})
+
+
+def receive_message(request):
+    next = request.POST.get('next', '/')
+    next = next + "#footer"
+    form = MessageForm(request.POST)
+    if form.is_valid():
+        form.save()
+    else:
+        return HttpResponseBadRequest("Bad Request")
+    return HttpResponseRedirect(next)
