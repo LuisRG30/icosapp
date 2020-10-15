25c796869736a69eda72133f7e1a16df30acd6b1
LuisRG30 <58560972+LuisRG30@users.noreply.github.com>
2020-10-14 21:58:53 -0500

    Ready for launch

--- Icosapp/settings.py
+++ Icosapp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%q2d_e$jh^5=ep$0syna4x+n(fo*b)wc=ort9j7^al2uc0i*$x'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
